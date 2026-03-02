d8755308e8b23d46c20fc8ecfc885fe5421a2241
biodata-phy <phyfiq@gmail.com>
2026-03-03 00:22:17 +0700

    fix: Update Vite base path from absolute to relative.

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/diaz-ataya/', // <--- TAMBAHKAN BARIS INI
+  base: './diaz-ataya/', // <--- TAMBAHKAN BARIS INI
 })
