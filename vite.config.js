516ad42280677ee55f4e3fc896319e2cad02a17b
biodata-phy <phyfiq@gmail.com>
2026-03-03 00:01:17 +0700

    fix: use relative paths for better portability across github pages settings

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/diaz-ataya/', // Sesuaikan dengan nama repo Anda
+  base: './', // Menggunakan path relatif agar lebih aman di GitHub Pages
   plugins: [react()],
 })
