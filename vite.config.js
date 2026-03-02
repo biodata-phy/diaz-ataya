e82f954b0061ad55fa2ebacfccb99cc794efe6ea
biodata-phy <phyfiq@gmail.com>
2026-03-03 00:15:27 +0700

    chore: Set Vite base path to `/diaz-ataya/` in Vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Menggunakan path relatif agar lebih aman di GitHub Pages
   plugins: [react()],
+  base: '/diaz-ataya/', // <--- TAMBAHKAN BARIS INI
 })
